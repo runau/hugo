ed21beac0729af908a4dd3a7f902863f060005eb
runau <runa@engineergirl.work>
2020-03-20 16:18:42 +0900

    最新commit

--- tweetBlog/build.sh
+++ tweetBlog/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+cp ./public/* ./public_bk/ -rp
+
 dir_path="content/posts/*"
-dirs=`find $dir_path -maxdepth 0 -type f -name 20200307*.md`
+dirs=`find $dir_path -maxdepth 0 -type f -name $1*.md`
 
 for dir in $dirs;
 do
@@ -12,3 +14,11 @@ done
 hugo -t zzo_custom
 cp ./content/posts/meta_image/* ./public/posts/meta_image/ -p
 cp ./content/posts/img/* ./public/posts/img/ -p
+
+rm ./public/*/*/index.json
+rm ./public/*/index.json
+rm ./public/index.json
+python diff.py
+
+aws s3 sync public s3://linebot-runauna/blog/
+aws cloudfront create-invalidation --distribution-id E4NBTPF3DAIQ2 --paths "/blog/*"
